abdc048514f9d1e77f088b1153c8cd61ca59d2e9
Loann Peurey <loannpeurey@orange.fr>
2020-10-01 11:43:14 +0200

    first application spring boot hello world

---
+++ src/test/java/fr/utbm/TA72brainstorming/Ta72BrainstormingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.utbm.TA72brainstorming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta72BrainstormingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
